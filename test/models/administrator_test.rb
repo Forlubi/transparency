f2aa77f6a0c32c56daf614594b833106277bf46e
youbinaa <youbina@brandeis.edu>
2020-02-23 17:06:31 -0500

    Make a basic Administrator model (including secure passwords)

--- test/models/administrator_test.rb
+++ test/models/administrator_test.rb
@@ -2,63 +2,63 @@ require 'test_helper'
 
 class AdministratorTest < ActiveSupport::TestCase
   def setup
-    @admin = Administrator.new(name: "Example Admin", email: "admin@example.com",
+    @administrator = Administrator.new(name: "Example Admin", email: "admin@example.com",
                                password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
-    assert @admin.valid?
+    assert @administrator.valid?
   end
 
   test "name should be present" do
-    @admin.name = "    "
-    assert_not @admin.valid?
+    @administrator.name = "    "
+    assert_not @administrator.valid?
   end
 
   test "email should be present" do
-    @admin.email = "     "
-    assert_not @admin.valid?
+    @administrator.email = "     "
+    assert_not @administrator.valid?
   end
 
   test "name should not be too long" do
-    @admin.name = "a" * 51
-    assert_not @admin.valid?
+    @administrator.name = "a" * 51
+    assert_not @administrator.valid?
   end
 
   test "email should not be too long" do
-    @admin.email = "a" * 244 + "@example.com"
-    assert_not @admin.valid?
+    @administrator.email = "a" * 244 + "@example.com"
+    assert_not @administrator.valid?
   end
 
   test "email validation should reject invalid addresses" do
     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
                            foo@bar_baz.com foo@bar+baz.com]
     invalid_addresses.each do |invalid_address|
-      @admin.email = invalid_address
-      assert_not @admin.valid?, "#{invalid_address.inspect} should be invalid"
+      @administrator.email = invalid_address
+      assert_not @administrator.valid?, "#{invalid_address.inspect} should be invalid"
     end
   end
 
   test "email addresses should be unique" do
-    duplicate_admin = @admin.dup
-    @admin.save
+    duplicate_admin = @administrator.dup
+    @administrator.save
     assert_not duplicate_admin.valid?
   end
 
   test "email addresses should be saved as lower-case" do
     mixed_case_email = "Foo@ExAMPle.CoM"
-    @admin.email = mixed_case_email
-    @admin.save
-    assert_equal mixed_case_email.downcase, @admin.reload.email
+    @administrator.email = mixed_case_email
+    @administrator.save
+    assert_equal mixed_case_email.downcase, @administrator.reload.email
   end
 
   test "password should be present (nonblank)" do
-    @admin.password = @admin.password_confirmation = " " * 6
-    assert_not @admin.valid?
+    @administrator.password = @administrator.password_confirmation = " " * 6
+    assert_not @administrator.valid?
   end
 
   test "password should have a minimum length" do
-    @admin.password = @admin.password_confirmation = "a" * 5
-    assert_not @admin.valid?
+    @administrator.password = @administrator.password_confirmation = "a" * 5
+    assert_not @administrator.valid?
   end
 end
