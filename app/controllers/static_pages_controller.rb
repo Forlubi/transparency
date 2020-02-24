db4e12e0a16a35216b1e4d50973adf29814ec299
Anyan <anyanxie@gmail.com>
2020-02-24 12:32:53 -0500

    created header and footer

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,4 +2,13 @@ class StaticPagesController < ApplicationController
   def home
     
   end
+
+  def info
+  end
+
+  def news
+  end
+
+  def feedback
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
   resources :study_areas
   resources :sensors
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'static_pages#home'
+  get  '/info',    to: 'static_pages#info'
+  get  '/news',    to: 'static_pages#news'
+  get  '/feedback',    to: 'static_pages#feedback'
 end
